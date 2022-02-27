dfb34a12b5aaf42cd8ccbbe44659fbcef80e4f01
tilakdkumar <tilak.kumar1997@gmail.com>
2022-02-27 17:34:05 +0530

    kepler-scope data

---
+++ index.js
@@ -0,0 +1,15 @@
+const parse = require("csv-parse");
+const fs = require("fs");
+
+const result = [];
+fs.createReadStream("kepler_scope.csv")
+  .on("data", (data) => {
+    result.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(result);
+    console.log(" done ");
+  });
