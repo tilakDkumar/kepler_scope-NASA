314bd813077d21133fe4185702317cda789362bc
tilakdkumar <tilak.kumar1997@gmail.com>
2022-03-08 21:58:49 +0530

    collections of data

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 const parse = require("csv-parse");
 const fs = require("fs");
-console.log(parse)
 const result = [];
+
+const habitablePlanets = []
+function isHabitablePlanets(plane){
+    return plane['koi_disposition'] == 'CONFIRMED' && 
+    plane['koi_insol'] > 0.36 && plane['koi_insol'] < 1.11 
+    && plane['koi_prad'] < 1.6
+}
  fs.createReadStream("kepler_scope.csv")
  .pipe(parse.parse({
      columns: true,
      comment:'#'
  }))
   .on("data", (data) => {
-    result.push(data);
+    if(isHabitablePlanets(data)){
+      habitablePlanets.push(data)
+    }
+    // result.push(data);
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(result);
+    // console.log(result);
+    console.log(habitablePlanets);
+
     console.log(" done ");
   });
  
